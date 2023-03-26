76f98037a3c27f9c700211be82453e6d5ab0df77
RyoSogawa <koashimitekara.1122@gmail.com>
2023-03-26 22:58:47 +0900

    fix: redeploy wasn't working

--- example/redeploy.sh
+++ example/redeploy.sh
@@ -4,12 +4,12 @@
 yarn push
 
 # get last deployment id
-LAST_DEPLOYMENT_ID=$( clasp deployments | pcregrep -o1 '\- ([A-Za-z0-9\-\_]+) @\d+ - web app meta-version' )
+LAST_DEPLOYMENT_ID=$( yarn clasp deployments | pcregrep -o1 '\- ([A-Za-z0-9\-\_]+) @\d+ - web app meta-version' )
 
 if [ -z "$LAST_DEPLOYMENT_ID" ];then
-    LAST_DEPLOYMENT_ID=$( clasp deployments | tail -1 | pcregrep -o1 '\- ([A-Za-z0-9\-\_]+)' )
+    LAST_DEPLOYMENT_ID=$( yarn clasp deployments | tail -1 | pcregrep -o1 '\- ([A-Za-z0-9\-\_]+)' )
 fi
 
 
 # deploy
-clasp deploy --deploymentId $LAST_DEPLOYMENT_ID
+yarn clasp deploy --deploymentId "$LAST_DEPLOYMENT_ID"
