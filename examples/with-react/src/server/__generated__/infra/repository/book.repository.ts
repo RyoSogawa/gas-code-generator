0ae329a7acab6c20c744e2d962bfe0702f16fcbe
RyoSogawa <koashimitekara.1122@gmail.com>
2023-03-28 22:53:20 +0900

    feat: convert fields for json

--- examples/with-react/src/server/__generated__/infra/repository/book.repository.ts
+++ examples/with-react/src/server/__generated__/infra/repository/book.repository.ts
@@ -1,3 +1,5 @@
+import { convertArrayToJsonCompatible } from '@gas-code-generator/utils';
+
 import type { IBookRepository } from '@/server/__generated__/domain/repository/book.repository';
 
 import { Book } from '@/models/__generated__/domain/entity/book.entity';
@@ -9,14 +11,14 @@ export class BookRepository implements IBookRepository {
     const lastRow = sheet.getLastRow();
     const lastColumn = sheet.getLastColumn();
     const allBooks = sheet.getRange(2, 1, lastRow - 1, lastColumn).getValues();
-    return allBooks.map(
-      (book, index) =>
-        new Book(
-          index,
-          // @ts-ignore
-          ...book,
-        ),
-    );
+    return allBooks.map((book, index) => {
+      const fields = convertArrayToJsonCompatible(book);
+      return new Book(
+        index,
+        // @ts-ignore
+        ...fields,
+      );
+    });
   }
 
   public findById(id: string): Book | null {
@@ -28,11 +30,12 @@ export class BookRepository implements IBookRepository {
     const index = allBooks.findIndex((value) => value[0] === id);
     if (index === -1) return null;
     const book = allBooks[index];
+    const fields = convertArrayToJsonCompatible(book);
 
     return new Book(
       index,
       // @ts-ignore
-      ...book,
+      ...fields,
     );
   }
 
