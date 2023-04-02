bd38bc651399341e9d1bd66af9454f053e29e7a0
RyoSogawa <koashimitekara.1122@gmail.com>
2023-04-02 17:49:01 +0900

    chore: add vite-tsconfig-paths

--- examples/with-react/src/client/vite.config.ts
+++ examples/with-react/src/client/vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { viteSingleFile } from 'vite-plugin-singlefile';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [react(), viteSingleFile(), tsconfigPaths()],
   build: {
     outDir: '../../dist',
   },
